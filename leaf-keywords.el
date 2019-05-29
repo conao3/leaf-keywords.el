90085e14469089c88d895abbed8b63ef98e5a896
conao3 <conao3@gmail.com>
2019-05-30 04:34:17 +0900

    add Commentary section

--- leaf-keywords.el
+++ leaf-keywords.el
@@ -25,7 +25,10 @@
 
 ;;; Commentary:
 
-;; simpify init.el
+;; leaf-keywords.el provide additional keywords for leaf.el
+;; This package defines keywords that are dependent on an external package.
+;;
+;; More information is [[https://github.com/conao3/leaf-keywords.el][here]]
 
 ;;; Code:
 
