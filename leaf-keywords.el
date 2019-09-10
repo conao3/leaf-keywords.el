6699297908d6b508a72d90bb7d77b4e09eb8775a
conao3 <conao3@gmail.com>
2019-09-11 01:50:21 +0900

    tagged v1.2.5

--- leaf-keywords.el
+++ leaf-keywords.el
@@ -5,7 +5,7 @@
 ;; Author: Naoya Yamashita <conao3@gmail.com>
 ;; Maintainer: Naoya Yamashita <conao3@gmail.com>
 ;; Keywords: lisp settings
-;; Version: 1.2.4
+;; Version: 1.2.5
 ;; URL: https://github.com/conao3/leaf-keywords.el
 ;; Package-Requires: ((emacs "24.4") (leaf "3.1.0"))
 
