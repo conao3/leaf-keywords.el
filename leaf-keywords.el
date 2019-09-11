5df7a91727a3ab15302a0e7d36bdf6d075fb18be
conao3 <conao3@gmail.com>
2019-09-11 23:39:26 +0900

    fix license statement

--- leaf-keywords.el
+++ leaf-keywords.el
@@ -16,11 +16,12 @@
 
 ;; This program is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Affero
-;; GNU General Public License for more details.
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;; See the Affero GNU General Public License for more details.
 
-;; You should have received a copy of the Affero GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;; You should have received a copy of the Affero GNU General Public
+;; License along with this program.
+;; If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
