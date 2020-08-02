c2b165a81ebe63a6b9f6cbe2f9d9a8b454421580
ROCKTAKEY <rocktakey@gmail.com>
2020-08-02 17:01:16 +0900

    Move normalizer of :grugru.
    Co-authored-by: Naoya Yamashita <conao3@gmail.com>

--- leaf-keywords.el
+++ leaf-keywords.el
@@ -139,20 +139,7 @@
    :chord*     (progn
                  (leaf-register-autoload (cadr leaf--value) leaf--name)
                  `((leaf-key-chords* ,(car leaf--value)) ,@leaf--body))
-   :grugru     `((grugru-define-multiple ,@(mapcar
-                                            (lambda (arg)
-                                              (if (grugru--strings-or-function-p (cdr arg))
-                                                  (list leaf--name arg)
-                                                arg))
-                                            (if (or
-                                                 (grugru--strings-or-function-p (cdar leaf--value))
-                                                 (and
-                                                  (not
-                                                   (or (grugru--strings-or-function-p (cdr-safe (caar leaf--value)))
-                                                       (grugru--strings-or-function-p (cdr-safe (car-safe (cdr-safe (caar leaf--value)))))))
-                                                  (grugru--strings-or-function-p (cdar (cdar leaf--value)))))
-                                                leaf--value (car leaf--value))))
-                 ,@leaf--body)
+   :grugru     `((grugru-define-multiple ,@leaf--value) ,@leaf--body)
    :mode-hook  `(,@(mapcar (lambda (elm) `(leaf-keywords-handler-mode-hook ,leaf--name ,(car elm) ,@(cadr elm))) leaf--value) ,@leaf--body))
   "Additional `leaf-keywords' before wait loading.
 :after ... <this place> :leaf-defer"
@@ -351,6 +338,21 @@
         (lambda (elm)
           (if (eq t elm) leaf--name elm))
         leaf--value)))
+
+    ((memq leaf--key '(:grugru))
+     (mapcar
+      (lambda (arg)
+        (if (grugru--strings-or-function-p (cdr arg))
+            (list leaf--name arg)
+          arg))
+      (if (or
+           (grugru--strings-or-function-p (cdar leaf--value))
+           (and
+            (not
+             (or (grugru--strings-or-function-p (cdr-safe (caar leaf--value)))
+                 (grugru--strings-or-function-p (cdr-safe (car-safe (cdr-safe (caar leaf--value)))))))
+            (grugru--strings-or-function-p (cdar (cdar leaf--value)))))
+          leaf--value (car leaf--value))))
 
     ((memq leaf--key '(:mode-hook))
      (mapcar
