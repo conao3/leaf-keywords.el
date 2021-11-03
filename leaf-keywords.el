2ccda1629261a57ecedb1514dcb656fd238b5f01
fernandodemorais-jf <47619475+fernandodemorais-jf@users.noreply.github.com>
2021-11-02 22:54:44 -0300

    Allows already installed packages to be loaded.

--- leaf-keywords.el
+++ leaf-keywords.el
@@ -101,7 +101,7 @@
 (defcustom leaf-keywords-after-conditions
   (leaf-list
    :feather    `(,@(mapcar (lambda (elm) `(leaf-handler-package ,leaf--name ,(car elm) ,(cdr elm))) leaf--value)
-                 (feather-add-after-installed-hook-sexp ,(caar (last leaf--value)) ,@leaf--body))
+                 (eval (feather-add-after-installed-hook-sexp ,(caar (last leaf--value)) ,@leaf--body)))
    :straight   `(,@(mapcar (lambda (elm) `(straight-use-package ',elm)) leaf--value) ,@leaf--body)
    :el-get     `(,@(mapcar (lambda (elm) `(el-get-bundle ,@elm)) leaf--value) ,@leaf--body)
    :ensure-system-package
